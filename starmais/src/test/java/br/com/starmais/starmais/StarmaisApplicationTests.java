b80148408c738c4bc02f9e803069cbed96523aaf
Miguel Micoski <micoskijr@hotmail.com>
2024-04-05 20:34:57 -0300

    Inicio do projeto Spring

---
+++ starmais/src/test/java/br/com/starmais/starmais/StarmaisApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.starmais.starmais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarmaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
